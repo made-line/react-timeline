275a6793c77144e58ff65f926a00a8ec5671fb5b
Madeline <madelign@gmail.com>
2020-12-28 21:37:22 -0800

    finished TimelineEvent component. passes wave 1 tests

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
+
+
+  const exampleList = {
+    "person": "Ada Lovelace",
+    "events": [
+      {
+        "person": "Adele Goldberg",
+        "status": "In Smalltalk, everything happens somewhere else.",
+        "timeStamp": "2018-05-18T22:12:03Z"
+      },
+      {
+        "person": "Erica Baker",
+        "status": "Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention.",
+        "timeStamp": "2018-05-18T22:19:40Z"
+      },
+    ]
+    }
+
 
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">Timeline</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent test={exampleList}/>
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+
+const TimelineEvent = (props) => {
+  return(
+    <div className="TimelineEvent">
+    <ul>
+    <li>person: {props.person}</li>
+    <li>status: {props.status}</li>
+    <li>timestamp: <Timestamp time={props.timeStamp} /></li>
+    </ul>
+
+    </div>
+  );
+};
+
+TimelineEvent.propTypes = {
+  person: PropTypes.string,
+  status: PropTypes.string,
+  timeStamp: PropTypes.string,
 }
 
 export default TimelineEvent;
